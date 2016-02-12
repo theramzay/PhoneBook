6abcc495004cb9cc3748f8171cc8ff6205b80b33
Anton Burkin <ramzzzay@gmail.com>
2016-02-12 21:52:10 +0300

    Add: implementation of data base context

--- PhoneBook.Domain/Concrete/DBcon.cs
+++ PhoneBook.Domain/Concrete/DBcon.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PhoneBook.Domain.Entities;
 
 namespace PhoneBook.Domain.Concrete
 {
-    class DBcon
+    class DBcon : IdentityDbContext<User>
     {
+        public DBcon():base("DBcon", throwIfV1Schema:false)
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public static DBcon Create()
+        {
+            return new DBcon();
+        }
     }
 }
