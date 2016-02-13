c4cb9e15fecbffab0f6b8fe4471968af25e0b5ed
Anton Burkin <ramzzzay@gmail.com>
2016-02-13 17:40:15 +0300

    Add: User Managers

---
+++ PhoneBook.Domain/Infrastructure/ApplicationUserManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using PhoneBook.Domain.Entities;
+
+namespace PhoneBook.Domain.Infrastructure
+{
+    public class ApplicationUserManager : UserManager<User>
+    {
+        private IUserStore<User> _store;
+        private IUserTokenProvider<User, string> _userTokenProvider;
+
+        public ApplicationUserManager(IUserStore<User> store, IUserTokenProvider<User, string> userTokenProvider)
+            : base(store)
+        {
+            _store = store;
+            _userTokenProvider = userTokenProvider;
+        }
+    }
+}
---
+++ PhoneBook.Domain/Infrastructure/MainUserManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using PhoneBook.Domain.Entities;
+
+namespace PhoneBook.Domain.Infrastructure
+{
+    internal class MainUserManager
+    {
+        private readonly ApplicationUserManager _applicationUserManager;
+
+        public MainUserManager(ApplicationUserManager applicationUserManager)
+        {
+            _applicationUserManager = applicationUserManager;
+        }
+
+        public Task CreateAsync(string email, string password, bool isConfirmed = false)
+        {
+            return _applicationUserManager.CreateAsync(new User
+            {
+                Email = email,
+                Password = password,
+                EmailConfirmed = isConfirmed
+            }, password);
+        }
+
+        public Task ChangePasswordAsync(string email, string currentPassword, string newPassword)
+        {
+            return
+                _applicationUserManager.ChangePasswordAsync(
+                    _applicationUserManager.FindByEmailAsync(email).Id.ToString(), currentPassword, newPassword);
+        }
+    }
+}
