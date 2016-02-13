b0c3335a572d0bd6979c3166774fb63a5c37b61c
Anton Burkin <ramzzzay@gmail.com>
2016-02-13 18:48:54 +0300

    Update: project

--- PhoneBook.Core/Global.asax.cs
+++ PhoneBook.Core/Global.asax.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Autofac;
+using Autofac.Integration.WebApi;
 
 namespace PhoneBook.Core
 {
@@ -21,8 +23,11 @@ namespace PhoneBook.Core
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             var builder = new ContainerBuilder();
-            // Register your MVC controllers.
-            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            // Get your HttpConfiguration.
+            var config = GlobalConfiguration.Configuration;
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
         }
     }
 }
