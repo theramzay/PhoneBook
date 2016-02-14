5a1e80f6b266ce4b7d8f9203d78b4927479af53b
Anton Burkin <ramzzzay@gmail.com>
2016-02-15 00:05:34 +0300

    Add: first version at registration and and showing users in User Manager class

--- PhoneBook.Domain/Infrastructure/MainUserManager.cs
+++ PhoneBook.Domain/Infrastructure/MainUserManager.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 using PhoneBook.Domain.Abstract;
-using PhoneBook.Domain.Concrete;
 using PhoneBook.Domain.Entities;
 
 namespace PhoneBook.Domain.Infrastructure
@@ -14,19 +17,35 @@ namespace PhoneBook.Domain.Infrastructure
             _applicationUserManager = applicationUserManager;
         }
 
-        public async Task CreateAsync(string email, string password, bool isConfirmed = false)
+        public async Task<IdentityResult> CreateAsync(string email, string password, bool isConfirmed = false)
         {
-           await _applicationUserManager.CreateAsync(new User
+            var r = new Random();
+            var u = new User
             {
                 Email = email,
                 Password = password,
-                EmailConfirmed = isConfirmed
-            }, password);
+                Id = r.Next(1000000).ToString(),
+                HolidayTime = DateTime.Now,
+                BusinessTrip = false,
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                TwoFactorEnabled = true,
+                LockoutEndDateUtc = DateTime.Now,
+                LockoutEnabled = false,
+                AccessFailedCount = 1,
+                UserName = email
+            };
+            return await _applicationUserManager.CreateAsync(u, password);
         }
 
-        public Task ChangePasswordAsync(string email, string currentPassword, string newPassword)
+        public async Task<List<User>> ShowAsync()
         {
-            return
+            return await _applicationUserManager.Users.ToListAsync();
+        }
+
+        public async Task<IdentityResult> ChangePasswordAsync(string email, string currentPassword, string newPassword)
+        {
+            return await 
                 _applicationUserManager.ChangePasswordAsync(
                     _applicationUserManager.FindByEmailAsync(email).Id.ToString(), currentPassword, newPassword);
         }
