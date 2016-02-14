c77f37c5da1c5af736462b5e69dc8048b54519a0
Anton Burkin <ramzzzay@gmail.com>
2016-02-14 17:31:49 +0300

    Add: account creation

---
+++ PhoneBook.Core/Controllers/PhoneBookController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using PhoneBook.Core.Models;
+using PhoneBook.Domain.Abstract;
+
+namespace PhoneBook.Core.Controllers
+{
+    public class PhoneBookController : ApiController
+    {
+        private readonly IMainUserManager _mainUserManager;
+
+        public PhoneBookController(IMainUserManager mainUserManager)
+        {
+            _mainUserManager = mainUserManager;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Create(AccountRegistrationModel newUser)
+        {
+            if (!ModelState.IsValid) return BadRequest("Something frong with registration!");
+            _mainUserManager.CreateAsync(newUser.Email, newUser.Password);
+            return Ok("Registration complete");
+        }
+    }
+}
