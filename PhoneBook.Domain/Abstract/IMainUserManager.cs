51d37b383bb6cc1b4b0c7ca177fff448a4f1676c
Anton Burkin <ramzzzay@gmail.com>
2016-02-15 00:09:07 +0300

    Refactoring: new return types

--- PhoneBook.Domain/Abstract/IMainUserManager.cs
+++ PhoneBook.Domain/Abstract/IMainUserManager.cs
@@ -1,10 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using PhoneBook.Domain.Entities;
 
 namespace PhoneBook.Domain.Abstract
 {
     public interface IMainUserManager
     {
-        Task ChangePasswordAsync(string email, string currentPassword, string newPassword);
-        Task CreateAsync(string email, string password, bool isConfirmed = false);
+        Task<IdentityResult> ChangePasswordAsync(string email, string currentPassword, string newPassword);
+        Task<IdentityResult> CreateAsync(string email, string password, bool isConfirmed = false);
+        Task<List<User>> ShowAsync();
     }
 }
