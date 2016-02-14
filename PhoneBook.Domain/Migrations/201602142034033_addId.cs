91cb750556f4d6f1e600918e841fc7c58a7bf373
Anton Burkin <ramzzzay@gmail.com>
2016-02-14 23:50:42 +0300

    Add: another migration of ID

---
+++ PhoneBook.Domain/Migrations/201602142034033_addId.cs
@@ -0,0 +1,15 @@
+using System.Data.Entity.Migrations;
+
+namespace PhoneBook.Domain.Migrations
+{
+    public partial class addId : DbMigration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
