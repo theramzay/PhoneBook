26ae97a7f418306e6dbf8c698e245bd8861de70a
Anton Burkin <ramzzzay@gmail.com>
2016-02-16 20:42:59 +0300

    Add: test to claims

--- PhoneBook.Core/Controllers/ValuesController.cs
+++ PhoneBook.Core/Controllers/ValuesController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
 namespace PhoneBook.Core.Controllers
@@ -10,9 +6,10 @@ namespace PhoneBook.Core.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
+        [Authorize(Roles = "user")]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new[] {"value1", "value2"};
         }
 
         // GET api/values/5
@@ -22,12 +19,12 @@ namespace PhoneBook.Core.Controllers
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public void Post([FromBody] string value)
         {
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody] string value)
         {
         }
 
