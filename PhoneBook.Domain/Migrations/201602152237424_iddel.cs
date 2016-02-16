5fd5f472324370d42ce553646baf172cf290c489
Anton Burkin <ramzzzay@gmail.com>
2016-02-16 03:47:48 +0300

    Add: migrations

---
+++ PhoneBook.Domain/Migrations/201602152237424_iddel.cs
@@ -0,0 +1,16 @@
+namespace PhoneBook.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class iddel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ PhoneBook.Domain/Migrations/201602152255369_cleanupUserModel.cs
@@ -0,0 +1,18 @@
+namespace PhoneBook.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class cleanupUserModel : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.AspNetUsers", "Role");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.AspNetUsers", "Role", c => c.String());
+        }
+    }
+}
