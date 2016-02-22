d3e80b4abead709c16cb52023caabb66d8b47a12
Anton Burkin <ramzzzay@gmail.com>
2016-02-22 15:13:45 +0300

    Upgrade: bootstrap 3.0.0 => bootstrap 3.3.6

--- PhoneBook.Core/App_Start/BundleConfig.cs
+++ PhoneBook.Core/App_Start/BundleConfig.cs
@@ -17,11 +17,12 @@ namespace PhoneBook.Core
                 "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
+                "~/Scripts/bootstrap.min.js",
                 "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
+                "~/Content/bootstrap.min.css",
+                "~/Content/bootstrap-theme.min.css",
                 "~/Content/site.css"));
             bundles.Add(new ScriptBundle("~/bundles/sakura").Include("~/Scripts/Sakura.js"));
             bundles.Add(new ScriptBundle("~/bundles/reactJS").Include(
