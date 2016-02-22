c412d271f6a1adfc3ddf153091dad92586166b4d
Anton Burkin <ramzzzay@gmail.com>
2016-02-22 22:19:40 +0300

    Add: HTTPS to Phone Book Controller at core

--- PhoneBook.Core/Controllers/PhoneBookController.cs
+++ PhoneBook.Core/Controllers/PhoneBookController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Mvc;
 using PhoneBook.Core.Models;
 using PhoneBook.Domain.Abstract;
+using PhoneBook.Domain.Entities;
 
 namespace PhoneBook.Core.Controllers
 {
-    [RoutePrefix("api/PhoneBook")]
+    [RequireHttps]
+    [System.Web.Http.RoutePrefix("api/PhoneBook")]
     public class PhoneBookController : ApiController
     {
         private readonly IMainUserManager _mainUserManager;
@@ -15,17 +18,17 @@ namespace PhoneBook.Core.Controllers
             _mainUserManager = mainUserManager;
         }
 
-        [Route("Create")]
-        [HttpPost]
+        [System.Web.Http.Route("Create")]
+        [System.Web.Http.HttpPost]
         public async Task<IHttpActionResult> Create(AccountRegistrationModel newUser)
         {
             if (!ModelState.IsValid) return BadRequest("Something frong with registration!");
-            var response = await _mainUserManager.CreateAsync(newUser.Email, newUser.Password);
+            var response = await _mainUserManager.CreateAsync(new User {Email = newUser.Email,Password = newUser.Password});
             return Ok(new {Msg = response.Errors, IsOk = response.Succeeded});
         }
 
-        [Route("AddClaim")]
-        [HttpPost]
+        [System.Web.Http.Route("AddClaim")]
+        [System.Web.Http.HttpPost]
         public async Task<IHttpActionResult> AddClaim(ClaimAddModel c)
         {
             if (!ModelState.IsValid) return BadRequest("Something frong with adding claim to user!");
@@ -33,26 +36,26 @@ namespace PhoneBook.Core.Controllers
             return Ok(new {Msg = response.Errors, IsOk = response.Succeeded});
         }
 
-        [Route("All")]
-        [HttpGet]
+        [System.Web.Http.Route("All")]
+        [System.Web.Http.HttpGet]
         public async Task<IHttpActionResult> AllUsers()
         {
             var response = await _mainUserManager.ShowAsync();
             return Ok(response);
         }
 
-        [Route("ClaimTest")]
-        [HttpGet]
-        [Authorize(Roles = "user")]
+        [System.Web.Http.Route("ClaimTest")]
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Authorize(Roles = "user")]
         //[ClaimAuth(Role = "user")]
         public IHttpActionResult ClaimTest()
         {
             return Ok(new {Message = "Hello!", lol = "yep"});
         }
 
-        [Route("ClaimTestModer")]
-        [HttpGet]
-        [Authorize(Roles = "Moder")]
+        [System.Web.Http.Route("ClaimTestModer")]
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Authorize(Roles = "Moder")]
         //[ClaimAuth(Role = "user")]
         public IHttpActionResult ClaimTestModer()
         {
