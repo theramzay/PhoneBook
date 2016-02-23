653eb420b234dd8b719fc54e1f61978ada12174f
Anton Burkin <ramzzzay@gmail.com>
2016-02-24 01:50:48 +0300

    Add: new Main Page and Profile Page in SPA style

---
+++ PhoneBook.Core/Scripts/views/MainPage.jsx
@@ -0,0 +1,41 @@
+﻿var MainPage = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div className="jumbotron">
+          <h1>Greatest in world application for Big Bosses!</h1>
+          <p className="lead">Helpful application for every ruller who want's know everyrhing about he's employee.</p>
+          <p><a className="btn btn-primary btn-lg" id="tstbtnPlumb" href="#something">Learn more »</a></p>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <h2>Hello Boss!</h2>
+            <p>Our app will be useful for learning what's making you human!</p>
+            <p>
+                <a className="btn btn-default" href="#SomeInfo">Learn more »</a>
+            </p>
+          </div>
+          <div className="col-md-4">
+            <h2>It's easy, yep!</h2>
+            <p>After registration, u will be brought to amazing word of graphics and calanders and all this just with one-two clicks! Yep, this is real!</p>
+            <p>
+                <a className="btn btn-default" href="#ShowThis">Learn more »</a>
+            </p>
+          </div>
+          <div className="col-md-4">
+            <h2>Work - Everywhere!</h2>
+            <p>Application works on all actual platforms and browsers! U can enjoy us app from your gold iPhone of course!</p>
+            <p>
+                <a className="btn btn-default" href="#SomithingShow">Learn more »</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <MainPage />,
+  document.getElementById('content')
+);
---
+++ PhoneBook.Core/Scripts/views/ProfilePage.jsx
@@ -0,0 +1,35 @@
+﻿var Info = React.createClass({
+    loadFromServer: function () {
+        var self = this;
+        var tokenKey = "tokenInfo";
+        var token = sessionStorage.getItem(tokenKey);
+        $.ajax({
+            headers: {
+                'Authorization': 'bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            url: this.props.url
+        }).success(function (data) {
+            self.setState({
+                Email: data.Email
+            });
+        }).fail(function () {
+            alert("Error");
+        });
+    },
+    getInitialState: function () {
+        return {data: [] };
+    },
+    componentDidMount: function () {
+        this.loadFromServer();
+    },
+    render: function() {
+        return (
+          <div>
+              Yours email is - {this.state.Email} <br/>
+              Other content under hard development =)
+          </div>
+        );
+    }
+});
