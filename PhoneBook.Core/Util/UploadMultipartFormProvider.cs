d6e185626c557fccfaf587ccc8f224b53434136a
Anton Burkin <ramzzzay@gmail.com>
2016-03-03 15:51:30 +0300

    Add: custom provider to upload images

---
+++ PhoneBook.Core/Util/UploadMultipartFormProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace PhoneBook.Core.Util
+{
+    public class UploadMultipartFormProvider : MultipartFormDataStreamProvider
+    {
+        public UploadMultipartFormProvider(string rootPath) : base(rootPath)
+        {
+        }
+
+        public override string GetLocalFileName(HttpContentHeaders headers)
+        {
+            if (headers != null &&
+                headers.ContentDisposition != null)
+            {
+                return headers
+                    .ContentDisposition
+                    .FileName.TrimEnd('"').TrimStart('"');
+            }
+
+            return base.GetLocalFileName(headers);
+        }
+    }
+}
