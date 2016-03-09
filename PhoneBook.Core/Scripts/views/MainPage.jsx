8016c3d1f51fc994fe90fe56856f2303de9fbebd
Anton Burkin <ramzzzay@gmail.com>
2016-03-09 18:36:26 +0300

    Refactoring: main page UI to work with creative template

--- PhoneBook.Core/Scripts/views/MainPage.jsx
+++ PhoneBook.Core/Scripts/views/MainPage.jsx
@@ -2,34 +2,63 @@
   render: function() {
     return (
       <div>
-        <div className="jumbotron">
-          <h1>Greatest in world application for Big Bosses!</h1>
-          <p className="lead">Helpful application for every ruller who want's know everyrhing about he's employee.</p>
-          <p><a className="btn btn-primary btn-lg" id="tstbtnPlumb" href="#something">Learn more »</a></p>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <h2>Hello Boss!</h2>
-            <p>Our app will be useful for learning what's making you human!</p>
-            <p>
-                <a className="btn btn-default" href="#SomeInfo">Learn more »</a>
-            </p>
-          </div>
-          <div className="col-md-4">
-            <h2>It's easy, yep!</h2>
-            <p>After registration, u will be brought to amazing word of graphics and calanders and all this just with one-two clicks! Yep, this is real!</p>
-            <p>
-                <a className="btn btn-default" href="#ShowThis">Learn more »</a>
-            </p>
-          </div>
-          <div className="col-md-4">
-            <h2>Work - Everywhere!</h2>
-            <p>Application works on all actual platforms and browsers! U can enjoy us app from your gold iPhone of course!</p>
-            <p>
-                <a className="btn btn-default" href="#SomithingShow">Learn more »</a>
-            </p>
+
+      <section className="bg-primary" id="about">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 col-lg-offset-2 text-center">
+              <h2 className="section-heading">Greatest in world application for Big Bosses!</h2>
+              <hr className="light" />
+              <p className="text-faded">Helpful application for every ruller who want's know everyrhing about he's employee.</p>
+              <a className="btn btn-default btn-xl page-scroll" href="#">Get Started!</a>
+            </div>
           </div>
         </div>
+      </section>
+
+
+      <section id="services">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <h2 className="section-heading">At Your Service</h2>
+              <hr className="primary" />
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3 col-md-6 text-center">
+              <div className="service-box">
+                <i className="fa fa-4x fa-diamond wow bounceIn text-primary" />
+                <h3>Hello Boss!</h3>
+                <p className="text-muted">Our app will be useful for learning what's making you human!</p>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 text-center">
+              <div className="service-box">
+                <i className="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".1s" />
+                <h3>It's easy, yep!</h3>
+                <p className="text-muted">After registration, u will be brought to amazing word of graphics and calanders and all this just with one-two clicks! Yep, this is real!</p>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 text-center">
+              <div className="service-box">
+                <i className="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s" />
+                <h3>Work - Everywhere!</h3>
+                <p className="text-muted">Application works on all actual platforms and browsers! U can enjoy us app from your gold iPhone of course</p>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 text-center">
+              <div className="service-box">
+                <i className="fa fa-4x fa-heart wow bounceIn text-primary" data-wow-delay=".3s" />
+                <h3>Made with Love</h3>
+                <p className="text-muted">You have to make your work with love these days!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       </div>
     );
   }
