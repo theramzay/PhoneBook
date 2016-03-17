96da97a10feed293486a85b4650c6873c0ad72a8
Anton Burkin <ramzzzay@gmail.com>
2016-03-17 14:29:10 +0300

    Add: jquery cookie plugin

--- PhoneBook.Core/App_Start/BundleConfig.cs
+++ PhoneBook.Core/App_Start/BundleConfig.cs
@@ -10,6 +10,7 @@ namespace PhoneBook.Core
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.cookie-1.4.1.min.js",
                 "~/Scripts/dropzone/dropzone.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
