2cca9558ad85f2d424e1e47de1eb2be58a9109d2
Anton Burkin <ramzzzay@gmail.com>
2016-03-18 18:56:36 +0300

    Add: admin mode search results and moving from includes to indexOf

--- PhoneBook.Core/Scripts/views/SearchPage.jsx
+++ PhoneBook.Core/Scripts/views/SearchPage.jsx
@@ -23,13 +23,41 @@
         };
 
     },
+    SendToServer: function (es) {
+        es.preventDefault();
+        var tokenKey = "tokenInfo";
+        var token = $.cookie(tokenKey);
+        var data = {
+            FirstName: $("#FirstNameEdit").val(),
+            MiddleName: $("#MiddleNameEdit").val(),
+            LastName: $("#LastNameEdit").val(),
+            PhonePrivate: $("#PhonePrivateEdit").val(),
+            PhoneWork: $("#PhoneWorkEdit").val(),
+            Notes: $("#NotesEdit").val()
+        };
+        //console.log(data);
+        $.ajax({
+            headers: {
+                'Authorization': "bearer " + token
+            },
+            type: "POST",
+            url: this.props.url,
+            data: data
+        }).success(function (data) {
+            React.unmountComponentAtNode(document.getElementById('Settings'));
+            console.log(data);
+        }).fail(function (ee) {
+            alert(ee);
+        });
+    },
     componentWillReceiveProps: function() {
         this.loadFromServer();
         this.setState({ searchData: this.props.searchData, founded: [] });
     },
     render: function () {
         var claimsKey = "claims";
-        if ($.cookie(claimsKey).includes('Admin')) {
+        if (typeof $.cookie(claimsKey) === "undefined") $.cookie(claimsKey, "notauth");
+        if ($.cookie(claimsKey).indexOf("Admin") !== -1) {
             return (
             <div>
     {this.state.founded.map(function (user) {
@@ -39,12 +67,32 @@
         <img style={{ verticalAlign: "middle" }} src={user.PathToTmbOfPhoto} alt="user photo"/>
             </div>
             <div className="col-sm-4">
-                <h3>First Name: {user.FirstName} </h3>
-                <h3>Middle Name: {user.MiddleName}</h3>
-                <h3>Last Name: {user.LastName}</h3>
-                <h3>Notes: {user.Notes}</h3>
-                <h3>Work Phone: {user.PhoneWork}</h3>
-                <h3>YOUR ARE ADMIN</h3>
+                    <form onSubmit={this.SendToServer}>
+        <label htmlFor="FirstNameEdit">Enter First Name</label>
+        <input type="text" placeholder={user.FirstName}
+               id="FirstNameEdit" className="form-control" />
+
+        <label htmlFor="MiddleNameEdit">Enter Middle Name</label>
+        <input type="text" placeholder={user.MiddleName}
+               id="MiddleNameEdit" className="form-control" />
+
+        <label htmlFor="LastNameEdit">Enter Last Name</label>
+        <input type="text" placeholder={user.LastName}
+               id="LastNameEdit" className="form-control" />
+
+        <label htmlFor="PhonePrivateEdit">Enter your private phone</label>
+        <input type="text" placeholder={user.PhonePrivate}
+               id="PhonePrivateEdit" className="form-control" />
+
+        <label htmlFor="PhoneWorkEdit">Enter your work phone</label>
+        <input type="text" placeholder={user.PhoneWork}
+               id="PhoneWorkEdit" className="form-control" />
+
+        <label htmlFor="NotesEdit">Enter your note</label>
+        <input type="text" placeholder={user.Notes}
+               id="NotesEdit" className="form-control" />
+        <button className="btn btn-success" type="submit">Submit</button>
+                    </form>
             </div>
         </div>
     </div>);
