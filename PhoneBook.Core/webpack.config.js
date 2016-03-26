0c2db741d8d40924620a2b0857e06858ea7ab41d
Anton Burkin <ramzzzay@gmail.com>
2016-03-26 18:22:26 +0300

    Add: webpack.config and webpack-notifier

---
+++ PhoneBook.Core/webpack.config.js
@@ -0,0 +1,30 @@
+﻿/// <binding ProjectOpened='Watch - Development' />
+"use strict";
+var WebpackNotifierPlugin = require('webpack-notifier');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'Scripts');
+
+module.exports = {
+    entry: {
+        application: './Scripts/app.jsx'
+    },
+    output: {
+        filename: "./Scripts/bundle.js"
+    },
+    module: {
+        loaders: [
+          {
+              test: /\.jsx?/,
+              include: APP_DIR,
+              loader: 'babel'
+          },
+          { test: /\.css$/, exclude: /\.useable\.css$/, loader: "style!css" },
+      { test: /\.useable\.css$/, loader: "style/useable!css" }
+        ]
+    },
+    plugins: [
+  new WebpackNotifierPlugin()
+    ]
+};
