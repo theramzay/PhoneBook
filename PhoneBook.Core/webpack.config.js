b134186016d2fa146b59e29ab9637fd9978db4c6
Anton Burkin <ramzzzay@gmail.com>
2016-03-27 13:17:02 +0300

    Refactoring: webpack.config to development mode

--- PhoneBook.Core/webpack.config.js
+++ PhoneBook.Core/webpack.config.js
@@ -1,14 +1,26 @@
-﻿/// <binding ProjectOpened='Watch - Development' />
+﻿/// <binding BeforeBuild='Run - Development' ProjectOpened='Watch - Development' />
 "use strict";
 var WebpackNotifierPlugin = require('webpack-notifier');
 var path = require('path');
+var webpack = require("webpack");
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'Scripts');
 
 module.exports = {
     entry: {
-        application: './Scripts/app.jsx'
+        application: [
+            //'./Scripts/jquery.cookie-1.4.1.min.js',
+            './Scripts/jquery.datetimepicker.full.min.js',
+            './Scripts/Parallax.js',
+            './Content/creative/bootstrap.min.js',
+            //'./Content/creative/jquery.easing.min.js',
+            './Content/creative/jquery.fittext.js',
+            './Content/creative/creative.js',
+            //'./Scripts/modernizr-2.6.2.js',
+            //'./Scripts/respond.js',
+            './Scripts/dropzone/dropzone.js',
+            './Scripts/views/App.jsx']
     },
     output: {
         filename: "./Scripts/bundle.js"
@@ -25,6 +37,15 @@ module.exports = {
         ]
     },
     plugins: [
-  new WebpackNotifierPlugin()
-    ]
+  new WebpackNotifierPlugin(),
+  new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      React: "react",
+      ReactDOM: "react-dom"
+  })
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
 };
