a05ebf42c4891c3ea634d3081b3deeaebf5d6588
Anton Burkin <ramzzzay@gmail.com>
2016-03-30 13:23:35 +0300

    Add: Upload image module

---
+++ PhoneBook.Core/Scripts/views/ImageUpload.jsx
@@ -0,0 +1,34 @@
+﻿module.exports = React.createClass({
+    getInitialState: function () {
+        return {};
+
+    },
+    componentDidMount: function () {
+        var url = this.props.url;
+        var tokenKey = "tokenInfo";
+        var token = $.cookie(tokenKey);
+        $("#dropForm").dropzone({
+            url: url, headers: {
+                'Authorization': "bearer " + token
+            }
+        }).on("success",()=> {
+            React.unmountComponentAtNode(document.getElementById('Settings')); //TODO: Make this WORK!
+            ReactDOM.render(
+<Info changed={true} url="api/Account/AllUserInfo"/>,
+            document.getElementById("content")
+        );
+    });
+},
+render: function () {
+    return (
+        <div>
+<form id="dropForm" onSubmit={this.SendToServer} className="dropzone">
+  <div className="fallback">
+    <input name="file" type="file" multiple={true} />
+  </div>
+</form>
+
+</div>
+        );
+}
+});
