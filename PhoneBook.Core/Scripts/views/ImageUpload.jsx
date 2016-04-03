94512c354f215bb369c060058c538e3a18b5d50a
Anton Burkin <ramzzzay@gmail.com>
2016-04-03 14:50:13 +0300

    Add: dropzone react component to Image Upload module

--- PhoneBook.Core/Scripts/views/ImageUpload.jsx
+++ PhoneBook.Core/Scripts/views/ImageUpload.jsx
@@ -1,6 +1,10 @@
-﻿module.exports = React.createClass({
+﻿var Dropzone = require('react-dropzone');
+
+module.exports = React.createClass({
     getInitialState: function () {
-        return {};
+        return {
+            files: []
+        };
 
     },
     componentDidMount: function () {
@@ -18,7 +22,16 @@
             document.getElementById("content")
         );
     });
-},
+    },
+    onDrop: function (files) {
+        this.setState({
+            files: files
+        });
+    },
+
+    onOpenClick: function () {
+        this.refs.dropzone.open();
+    },
 render: function () {
     return (
         <div>
@@ -26,6 +39,19 @@ render: function () {
   <div className="fallback">
     <input name="file" type="file" multiple={true} />
   </div>
+
+    <div>
+                <Dropzone ref="dropzone" onDrop={this.onDrop}>
+                    <div>Try dropping some files here, or click to select files to upload.</div>
+                </Dropzone>
+                <button type="button" onClick={this.onOpenClick}>
+                    Open Dropzone
+                </button>
+        {this.state.files.length > 0 ? <div>
+                <h2>Uploading {this.state.files.length} files...</h2>
+                <div>{this.state.files.map((file) => <img src={file.preview} /> )}</div>
+                </div> : null}
+    </div>
 </form>
 
 </div>
