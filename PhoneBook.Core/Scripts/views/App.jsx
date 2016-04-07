cd54ebf862f7fdf034b850c7160fb701f1e5c162
Anton Burkin <ramzzzay@gmail.com>
2016-04-07 13:42:15 +0300

    Add: fonts awesome to App.jsx

--- PhoneBook.Core/Scripts/views/App.jsx
+++ PhoneBook.Core/Scripts/views/App.jsx
@@ -5,6 +5,7 @@ var AddUser = require('./AddUser.jsx');
 var AuthUser = require('./AuthUser.jsx');
 var AuthUser = require('./AuthUser.jsx');
 var Info = require('./Info');
+require('font-awesome/css/font-awesome.css');
 
 ReactDOM.render(
   <MainPage />,
