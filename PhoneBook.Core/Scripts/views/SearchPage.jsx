4523d4d41e11dcaa516c2d8dbb41bfb2106f6c6d
Anton Burkin <ramzzzay@gmail.com>
2016-04-07 11:55:57 +0300

    Add: start time to date pickers in search admin mode & fix: picking right user to POST

--- PhoneBook.Core/Scripts/views/SearchPage.jsx
+++ PhoneBook.Core/Scripts/views/SearchPage.jsx
@@ -1,16 +1,17 @@
-﻿var DatePicker = require('react-datepicker');
-var moment = require('moment');
+﻿var DatePicker = require("react-datepicker");
+var moment = require("moment");
 
-require('react-datepicker/dist/react-datepicker.css');
+require("react-datepicker/dist/react-datepicker.css");
 
 module.exports = React.createClass({
-    getInitialState: function () {
+    getInitialState: function() {
         return {
             searchData: this.props.searchData,
             founded: [],
             firstSelectedDate: moment(),
-            secondSelectedDate: moment()
-        };
+            secondSelectedDate: moment(),
+            startDate: moment()
+    };
 
     },
     loadFromServer: function() {
@@ -24,28 +25,33 @@ module.exports = React.createClass({
             },
             type: "GET",
             url: this.props.url + "?searchData=" + this.state.searchData
-        }).success(function (data) {
+        }).success(function(data) {
             self.setState({
                 founded: data
             });
         });
     },
-    SendInfoToServer: function (e) {
+    sendInfoToServer: function(e) {
         //e.defaultPrevented();
         var tokenKey = "tokenInfo";
         var token = $.cookie(tokenKey);
+
         var data = {
-            Email: $("#EmailEdit").val(),
-            FirstName: $("#FirstNameEdit").val(),
-            MiddleName: $("#MiddleNameEdit").val(),
-            LastName: $("#LastNameEdit").val(),
-            PhonePrivate: $("#PhonePrivateEdit").val(),
-            PhoneWork: $("#PhoneWorkEdit").val(),
-            Notes: $("#NotesEdit").val(),
-            NotesForBoss: $("#NotesForBossEdit").val(),
-            HolidayTimeStart: this.state.firstSelectedDate,
-            HolidayTimeEnd: this.state.secondSelectedDate
+            Email: this.refs.EmailEdit.value,
+            FirstName: this.refs.FirstNameEdit.value,
+            LastName: this.refs.LastNameEdit.value,
+            MiddleName: this.refs.MiddleNameEdit.value,
+            PhonePrivate: this.refs.PhonePrivateEdit.value,
+            PhoneWork: this.refs.PhoneWorkEdit.value,
+            Notes: this.refs.NotesEdit.value,
+            NotesForBoss: this.refs.NotesForBossEdit.value,
+            HolidayTimeStart: this.state.firstSelectedDate.toJSON(),
+            HolidayTimeEnd: this.state.secondSelectedDate.toJSON()
         };
+        //["Email", "FirstName", "LastName", "MiddleName", "PhonePrivate", "PhoneWork", "Notes", "NotesForBoss"]
+        //    .forEach(function(l) {
+        //        //data[l] = $("#" + l + "Edit").val();
+        //    });
         console.log(data);
 
         $.ajax({
@@ -53,102 +59,94 @@ module.exports = React.createClass({
                 'Authorization': "bearer " + token
             },
             type: "POST",
-            url: '/api/Account/UpdateAllUserInfoByAdmin',
+            url: "/api/Account/UpdateAllUserInfoByAdmin",
             data: data
-        }).success(function (data) {
-            console.log(data);
-        }).fail(function (ee) {
-            console.log(ee);
+        }).success(function() {
+            console.log('ok');
+        }).fail(function(error) {
+            console.log('error: ',error.msg);
         });
     },
-    componentWillReceiveProps: function (newProp) {
+    componentWillReceiveProps: function(newProp) {
         this.setState({ searchData: newProp.searchData });
     },
-    componentDidUpdate: function (prevProps, prevState) {
+    componentDidUpdate: function(prevProps, prevState) {
         if (prevState.searchData !== this.state.searchData) {
             this.loadFromServer();
         }
     },
-    componentDidMount: function () {
+    componentDidMount: function() {
         this.loadFromServer();
     },
-    handleFirstDatePick: function (date) {
+    handleFirstDatePick: function(date) {
         this.setState({
             firstSelectedDate: date
         });
     },
-    handleSecondDatePick: function (date) {
+    handleSecondDatePick: function(date) {
         this.setState({
             secondSelectedDate: date
         });
     },
-    render: function () {
+    render: function() {
         var self = this;
         var claimsKey = "claims";
         if (typeof $.cookie(claimsKey) === "undefined") $.cookie(claimsKey, "notauth");
         if ($.cookie(claimsKey).indexOf("Admin") !== -1) {
             return (
-            <div>
-    {this.state.founded.map(function (user) {
-        return (<div id="TableOfSerach">
-<div className="row jumbotron">
-    <div className="col-sm-3">
-        <img style={{ verticalAlign: "middle" }} src={user.PathToTmbOfPhoto} alt="user photo"/>
+                <div>
+    {this.state.founded.map(function(user) {
+                    return (<div id="TableOfSerach">
+        <div className="row jumbotron">
+            <div className="col-sm-3">
+                <img style={{ verticalAlign: "middle" }} src={user.PathToTmbOfPhoto} alt="user photo"/>
             </div>
             <div className="col-sm-4">
-                    <form onSubmit={self.SendInfoToServer}>
-        <label htmlFor="EmailEdit">Enter Email</label>
-        <input type="text" placeholder={user.Email}
-               id="EmailEdit" className="form-control" value={user.Email} />
-        <label htmlFor="FirstNameEdit">Enter First Name</label>
-        <input type="text" placeholder={user.FirstName}
-               id="FirstNameEdit" className="form-control" />
+                <form onSubmit={self.sendInfoToServer}>
+                    <label htmlFor="EmailEdit">Enter Email</label>
+                    <input type="text" placeholder={user.Email} ref="EmailEdit" className="form-control" value={user.Email}/>
+                    <label htmlFor="FirstNameEdit">Enter First Name</label>
+                    <input type="text" placeholder={user.FirstName} ref="FirstNameEdit" className="form-control"/>
 
-        <label htmlFor="MiddleNameEdit">Enter Middle Name</label>
-        <input type="text" placeholder={user.MiddleName}
-               id="MiddleNameEdit" className="form-control" />
+                    <label htmlFor="MiddleNameEdit">Enter Middle Name</label>
+                    <input type="text" placeholder={user.MiddleName} ref="MiddleNameEdit" className="form-control"/>
 
-        <label htmlFor="LastNameEdit">Enter Last Name</label>
-        <input type="text" placeholder={user.LastName}
-               id="LastNameEdit" className="form-control" />
+                    <label htmlFor="LastNameEdit">Enter Last Name</label>
+                    <input type="text" placeholder={user.LastName} ref="LastNameEdit" className="form-control"/>
 
-        <label htmlFor="PhonePrivateEdit">Enter your private phone</label>
-        <input type="text" placeholder={user.PhonePrivate}
-               id="PhonePrivateEdit" className="form-control" />
+                    <label htmlFor="PhonePrivateEdit">Enter your private phone</label>
+                    <input type="text" placeholder={user.PhonePrivate} ref="PhonePrivateEdit" className="form-control"/>
 
-        <label htmlFor="PhoneWorkEdit">Enter your work phone</label>
-        <input type="text" placeholder={user.PhoneWork}
-               id="PhoneWorkEdit" className="form-control" />
+                    <label htmlFor="PhoneWorkEdit">Enter your work phone</label>
+                    <input type="text" placeholder={user.PhoneWork} ref="PhoneWorkEdit" className="form-control"/>
 
-        <label htmlFor="NotesEdit">Enter your note</label>
-        <input type="text" placeholder={user.Notes}
-               id="NotesEdit" className="form-control" />
-        <label htmlFor="NotesEdit">Enter boss note</label>
-        <input type="text" placeholder={user.NotesForBoss}
-               id="NotesForBossEdit" className="form-control" />
-        <label htmlFor="HolidayTimeStartEdit">Enter Holiday start time</label>
-        <DatePicker selected={self.state.firstSelectedDate} onChange={self.handleFirstDatePick} />
-        <label htmlFor="HolidayTimeEndEdit">Enter Holiday end time</label>
-        <DatePicker selected={self.state.secondSelectedDate} onChange={self.handleSecondDatePick} />
+                    <label htmlFor="NotesEdit">Enter your note</label>
+                    <input type="text" placeholder={user.Notes} ref="NotesEdit" className="form-control"/>
+                    <label htmlFor="NotesForBossEdit">Enter boss note</label>
+                    <input type="text" placeholder={user.NotesForBoss} ref="NotesForBossEdit" className="form-control"/>
+                    <label htmlFor="HolidayTimeStartEdit">Enter Holiday start time</label>
+                    <DatePicker startDate={self.state.startDate} dateFormat="MM/DD/YYYY" selected={self.state.firstSelectedDate} onChange={self.handleFirstDatePick}/>
+                    <label htmlFor="HolidayTimeEndEdit">Enter Holiday end time</label>
+                    <DatePicker startDate={self.state.startDate} dateFormat="MM/DD/YYYY" selected={self.state.secondSelectedDate} onChange={self.handleSecondDatePick}/>
 
-        <button className="btn btn-success" type="submit">Submit</button>
-                    </form>
+                    <button className="btn btn-success" type="submit">Submit</button>
+                </form>
             </div>
         </div>
     </div>);
-        })}
+                })}
 
 </div>
-        );
-            
+            );
+
         } else {
             return (
-            <div>
-    {this.state.founded.map(function (user) {
-        return (<div id="TableOfSerach">
-<div className="row jumbotron">
-    <div className="col-sm-3">
-        <img style={{ verticalAlign: "middle" }} src={user.PathToTmbOfPhoto} alt="user photo"/>
+                <div>
+    {this.state.founded.map(function(user) {
+                    return (<div id="TableOfSerach">
+        <div className="row jumbotron">
+            <div className="col-sm-3">
+                <img style={{ verticalAlign: "middle" }} src={user.PathToTmbOfPhoto} alt="user photo"/>
             </div>
             <div className="col-sm-4">
                 <h3>First Name: {user.FirstName} </h3>
@@ -159,11 +157,11 @@ module.exports = React.createClass({
             </div>
         </div>
     </div>);
-        })}
+                })}
 
 </div>
-        );
+            );
         }
-        
+
     }
 });
