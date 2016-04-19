da0cd08f828fe01b490f1c60e5e0c724ae4410d7
Anton Burkin <ramzzzay@gmail.com>
2016-04-19 17:24:31 +0300

    Add: registration in react-way

--- PhoneBook.Core/Scripts/views/buttons.jsx
+++ PhoneBook.Core/Scripts/views/buttons.jsx
@@ -13,6 +13,7 @@ module.exports = React.createClass({
         return {
             Email: "",
             Password: "",
+            ConfirmPassword: "",
             isAuth: false,
             userName: Cookie.load('userName')
         };
@@ -119,15 +120,91 @@ module.exports = React.createClass({
     clearForm: function () {
         this.setState({
             Email: "",
-            Password: ""
+            Password: "",
+            ConfirmPassword: ""
         });
     },
-    //
+    // log end
+
+    // Registration
+    submit: function (e) {
+        e.preventDefault();
+
+        console.log(this.state);
+        if (this.refs.PasswordReg.value === this.refs.ConfirmPassword.value) {
+            var data = {
+                Email: this.refs.Email.value,
+                Password: this.refs.PasswordReg.value,
+                ConfirmPassword: this.refs.ConfirmPassword.value
+            };
+
+            // Submit form via jQuery/AJAX
+            $.ajax({
+                type: "POST",
+                url: "api/Account/Register",
+                data: data
+            }).done(()=> {
+                    this.clearForm();
+                    $("#registrationModal").modal("hide");
+                })
+                .fail(function () {
+                    console.log("failed to register");
+                });
+        } else {
+            alert("Password are not equivalented");
+        }
+    },
+    checkFirst: function () {
+        var pass = $(this.refs.PasswordReg);
+        var repass = $(this.refs.ConfirmPassword);
+        console.log("check first pass", pass.val(), repass.val());
+        pass.setCustomValidity(pass.validity.patternMismatch ? pass.title : "");
+        if (pass.checkValidity()) repass.pattern = pass.value;
+    },
+    checkSecond: function () {
+        var repass = $(this.refs.ConfirmPassword);
+        console.log("check second pass", repass.val());
+        repass.setCustomValidity(repass.validity.patternMismatch ? repass.title : "");
+    },
+    // reg end
     render: function() {
         return (
             <div>
     <div id="registrationModal" className="modal fade" role="dialog">
-
+                    <div className="modal-dialog">
+    <div className="modal-content">
+        <div className="modal-header">
+            <button type="button" className="close" data-dismiss="modal">&times;</button>
+            <h4 className="modal-title">Registration</h4>
+        </div>
+        <div className="modal-body">
+            <form onSubmit={this.submit}>
+                <fieldset>
+                    <legend>Registration form</legend>
+                <input placeholder="Email" required={true}
+                       className="form-control" ref="Email"
+                       type="email" name="Email" label="Email:" />
+ <label>Enter password</label><br />
+ <input placeholder="Password"
+        onChange={this.checkFirst}
+        required={true} title="Password between 8 and 20 characters, including UPPER/lowercase, numbers and symbols"
+        pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$"
+        className="form-control" ref="PasswordReg" type="password" name="Password" label="Password:" />
+ <label>Re-enter password</label><br />
+ <input placeholder="Confirm password"
+        onChange={this.checkSecond}
+        required={true} title="Please enter the same Password as above"
+        pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$" className="form-control"
+        ref="ConfirmPassword" type="password" name="ConfirmPassword" label="ConfirmPassword:" />
+ <button className="btn btn-success" type="submit">Submit</button>
+                </fieldset>
+            </form>
+        </div>
+<div className="modal-footer">
+    <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+    </div>
+                    </div>
     </div>
     <div id="authorizationModal" className="modal fade" role="dialog">
                 <div className="modal-dialog">
