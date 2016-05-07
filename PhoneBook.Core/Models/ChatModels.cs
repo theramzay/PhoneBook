19f0f924afe067e1aa3285be0c9b95fafbb92031
Anton Burkin <ramzzzay@gmail.com>
2016-05-07 13:12:54 +0300

    Add: Messages model

---
+++ PhoneBook.Core/Models/ChatModels.cs
@@ -0,0 +1,17 @@
+﻿namespace PhoneBook.Core.Models
+{
+    public class ChatModels
+    {
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+
+    public class ChatMessages
+    {
+        public string Color { get; set; }
+        public string Message { get; set; }
+        public string Type { get; set; }
+        public string From { get; set; }
+    }
+}
--- PhoneBook.Core/Models/ChatUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PhoneBook.Core.Models
-{
-    public class ChatUser
-    {
-        public string ConnectionId { get; set; }
-        public string Name { get; set; }
-        public string Color { get; set; }
-    }
-}
