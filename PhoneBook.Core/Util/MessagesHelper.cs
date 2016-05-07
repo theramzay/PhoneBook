f94114fabd91fd656e33febccd447cf2c46ffacf
Anton Burkin <ramzzzay@gmail.com>
2016-05-07 13:12:29 +0300

    Add: Messages creator

---
+++ PhoneBook.Core/Util/MessagesHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Helpers;
+using PhoneBook.Core.Models;
+
+namespace PhoneBook.Core.Util
+{
+    public static class MessagesHelper
+    {
+        public static string Create(string color, string message, string type, string from)
+        {
+            var msg = new ChatMessages {Color = color, From = from, Type = type, Message = message};
+            var jsonMsg = Json.Encode(msg);
+            return jsonMsg;
+        }
+    }
+}
