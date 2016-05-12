6c3d3192474b02d1104ce5ad9c2bfd2e4de079be
Anton Burkin <ramzzzay@gmail.com>
2016-05-13 00:37:30 +0300

    Refactoring: Admin Page UI to react-way & remove jQuery

--- PhoneBook.Core/Assets/js/views/AdminPage.jsx
+++ PhoneBook.Core/Assets/js/views/AdminPage.jsx
@@ -8,20 +8,19 @@ module.exports = React.createClass({
     }
   },
   componentDidMount: function() {
-    $.ajax({
-      headers: {
-        'Authorization': "bearer " + Cookie.load('tokenInfo'),
-        'Content-Type': "application/json"
-      },
-      type: "GET",
-      url: this.props.url
-    }).success(users => {
-      let value = users.map(u => ({x:u.Email,y:new Date(u.DayOfRegistration)}))
-      this.setState({data: users.map(u => ({label:u.Email, values: value}))});
-      console.log(this.state.data);
-    }).fail(function () {
-      alert("Error");
-    });
+
+    fetch(this.props.url, {
+    method: 'GET',
+    headers: new Headers({
+      "Content-Type": "application/json",
+      "Authorization": "bearer " + Cookie.load('tokenInfo')
+    })
+  })
+  .then(r => r.json())
+  .then(users=>{
+    let value = users.map(u => ({x:u.Email,y:new Date(u.DayOfRegistration)}))
+    this.setState({data: users.map(u => ({label:u.Email, values: value}))});
+  });
   },
   render: function () {
     return (
