bc6878e4c8694ab135996e9fc79950ac7d3c58e6
Anton Burkin <ramzzzay@gmail.com>
2016-06-21 10:37:29 +0300

    Refactoring: XmlDocumentationProvider to pretty view

--- PhoneBook.Core/Areas/HelpPage/XmlDocumentationProvider.cs
+++ PhoneBook.Core/Areas/HelpPage/XmlDocumentationProvider.cs
@@ -10,19 +10,19 @@ using PhoneBook.Core.Areas.HelpPage.ModelDescriptions;
 namespace PhoneBook.Core.Areas.HelpPage
 {
     /// <summary>
-    /// A custom <see cref="IDocumentationProvider"/> that reads the API documentation from an XML documentation file.
+    ///     A custom <see cref="IDocumentationProvider" /> that reads the API documentation from an XML documentation file.
     /// </summary>
     public class XmlDocumentationProvider : IDocumentationProvider, IModelDocumentationProvider
     {
-        private XPathNavigator _documentNavigator;
         private const string TypeExpression = "/doc/members/member[@name='T:{0}']";
         private const string MethodExpression = "/doc/members/member[@name='M:{0}']";
         private const string PropertyExpression = "/doc/members/member[@name='P:{0}']";
         private const string FieldExpression = "/doc/members/member[@name='F:{0}']";
         private const string ParameterExpression = "param[@name='{0}']";
+        private readonly XPathNavigator _documentNavigator;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="XmlDocumentationProvider"/> class.
+        ///     Initializes a new instance of the <see cref="XmlDocumentationProvider" /> class.
         /// </summary>
         /// <param name="documentPath">The physical path to XML document.</param>
         public XmlDocumentationProvider(string documentPath)
@@ -31,32 +31,34 @@ namespace PhoneBook.Core.Areas.HelpPage
             {
                 throw new ArgumentNullException("documentPath");
             }
-            XPathDocument xpath = new XPathDocument(documentPath);
+            var xpath = new XPathDocument(documentPath);
             _documentNavigator = xpath.CreateNavigator();
         }
 
         public string GetDocumentation(HttpControllerDescriptor controllerDescriptor)
         {
-            XPathNavigator typeNode = GetTypeNode(controllerDescriptor.ControllerType);
+            var typeNode = GetTypeNode(controllerDescriptor.ControllerType);
             return GetTagValue(typeNode, "summary");
         }
 
         public virtual string GetDocumentation(HttpActionDescriptor actionDescriptor)
         {
-            XPathNavigator methodNode = GetMethodNode(actionDescriptor);
+            var methodNode = GetMethodNode(actionDescriptor);
             return GetTagValue(methodNode, "summary");
         }
 
         public virtual string GetDocumentation(HttpParameterDescriptor parameterDescriptor)
         {
-            ReflectedHttpParameterDescriptor reflectedParameterDescriptor = parameterDescriptor as ReflectedHttpParameterDescriptor;
+            var reflectedParameterDescriptor = parameterDescriptor as ReflectedHttpParameterDescriptor;
             if (reflectedParameterDescriptor != null)
             {
-                XPathNavigator methodNode = GetMethodNode(reflectedParameterDescriptor.ActionDescriptor);
+                var methodNode = GetMethodNode(reflectedParameterDescriptor.ActionDescriptor);
                 if (methodNode != null)
                 {
-                    string parameterName = reflectedParameterDescriptor.ParameterInfo.Name;
-                    XPathNavigator parameterNode = methodNode.SelectSingleNode(String.Format(CultureInfo.InvariantCulture, ParameterExpression, parameterName));
+                    var parameterName = reflectedParameterDescriptor.ParameterInfo.Name;
+                    var parameterNode =
+                        methodNode.SelectSingleNode(string.Format(CultureInfo.InvariantCulture, ParameterExpression,
+                            parameterName));
                     if (parameterNode != null)
                     {
                         return parameterNode.Value.Trim();
@@ -69,31 +71,33 @@ namespace PhoneBook.Core.Areas.HelpPage
 
         public string GetResponseDocumentation(HttpActionDescriptor actionDescriptor)
         {
-            XPathNavigator methodNode = GetMethodNode(actionDescriptor);
+            var methodNode = GetMethodNode(actionDescriptor);
             return GetTagValue(methodNode, "returns");
         }
 
         public string GetDocumentation(MemberInfo member)
         {
-            string memberName = String.Format(CultureInfo.InvariantCulture, "{0}.{1}", GetTypeName(member.DeclaringType), member.Name);
-            string expression = member.MemberType == MemberTypes.Field ? FieldExpression : PropertyExpression;
-            string selectExpression = String.Format(CultureInfo.InvariantCulture, expression, memberName);
-            XPathNavigator propertyNode = _documentNavigator.SelectSingleNode(selectExpression);
+            var memberName = string.Format(CultureInfo.InvariantCulture, "{0}.{1}", GetTypeName(member.DeclaringType),
+                member.Name);
+            var expression = member.MemberType == MemberTypes.Field ? FieldExpression : PropertyExpression;
+            var selectExpression = string.Format(CultureInfo.InvariantCulture, expression, memberName);
+            var propertyNode = _documentNavigator.SelectSingleNode(selectExpression);
             return GetTagValue(propertyNode, "summary");
         }
 
         public string GetDocumentation(Type type)
         {
-            XPathNavigator typeNode = GetTypeNode(type);
+            var typeNode = GetTypeNode(type);
             return GetTagValue(typeNode, "summary");
         }
 
         private XPathNavigator GetMethodNode(HttpActionDescriptor actionDescriptor)
         {
-            ReflectedHttpActionDescriptor reflectedActionDescriptor = actionDescriptor as ReflectedHttpActionDescriptor;
+            var reflectedActionDescriptor = actionDescriptor as ReflectedHttpActionDescriptor;
             if (reflectedActionDescriptor != null)
             {
-                string selectExpression = String.Format(CultureInfo.InvariantCulture, MethodExpression, GetMemberName(reflectedActionDescriptor.MethodInfo));
+                var selectExpression = string.Format(CultureInfo.InvariantCulture, MethodExpression,
+                    GetMemberName(reflectedActionDescriptor.MethodInfo));
                 return _documentNavigator.SelectSingleNode(selectExpression);
             }
 
@@ -102,12 +106,13 @@ namespace PhoneBook.Core.Areas.HelpPage
 
         private static string GetMemberName(MethodInfo method)
         {
-            string name = String.Format(CultureInfo.InvariantCulture, "{0}.{1}", GetTypeName(method.DeclaringType), method.Name);
-            ParameterInfo[] parameters = method.GetParameters();
+            var name = string.Format(CultureInfo.InvariantCulture, "{0}.{1}", GetTypeName(method.DeclaringType),
+                method.Name);
+            var parameters = method.GetParameters();
             if (parameters.Length != 0)
             {
-                string[] parameterTypeNames = parameters.Select(param => GetTypeName(param.ParameterType)).ToArray();
-                name += String.Format(CultureInfo.InvariantCulture, "({0})", String.Join(",", parameterTypeNames));
+                var parameterTypeNames = parameters.Select(param => GetTypeName(param.ParameterType)).ToArray();
+                name += string.Format(CultureInfo.InvariantCulture, "({0})", string.Join(",", parameterTypeNames));
             }
 
             return name;
@@ -117,7 +122,7 @@ namespace PhoneBook.Core.Areas.HelpPage
         {
             if (parentNode != null)
             {
-                XPathNavigator node = parentNode.SelectSingleNode(tagName);
+                var node = parentNode.SelectSingleNode(tagName);
                 if (node != null)
                 {
                     return node.Value.Trim();
@@ -129,25 +134,26 @@ namespace PhoneBook.Core.Areas.HelpPage
 
         private XPathNavigator GetTypeNode(Type type)
         {
-            string controllerTypeName = GetTypeName(type);
-            string selectExpression = String.Format(CultureInfo.InvariantCulture, TypeExpression, controllerTypeName);
+            var controllerTypeName = GetTypeName(type);
+            var selectExpression = string.Format(CultureInfo.InvariantCulture, TypeExpression, controllerTypeName);
             return _documentNavigator.SelectSingleNode(selectExpression);
         }
 
         private static string GetTypeName(Type type)
         {
-            string name = type.FullName;
+            var name = type.FullName;
             if (type.IsGenericType)
             {
                 // Format the generic type name to something like: Generic{System.Int32,System.String}
-                Type genericType = type.GetGenericTypeDefinition();
-                Type[] genericArguments = type.GetGenericArguments();
-                string genericTypeName = genericType.FullName;
+                var genericType = type.GetGenericTypeDefinition();
+                var genericArguments = type.GetGenericArguments();
+                var genericTypeName = genericType.FullName;
 
                 // Trim the generic parameter counts from the name
                 genericTypeName = genericTypeName.Substring(0, genericTypeName.IndexOf('`'));
-                string[] argumentTypeNames = genericArguments.Select(t => GetTypeName(t)).ToArray();
-                name = String.Format(CultureInfo.InvariantCulture, "{0}{{{1}}}", genericTypeName, String.Join(",", argumentTypeNames));
+                var argumentTypeNames = genericArguments.Select(t => GetTypeName(t)).ToArray();
+                name = string.Format(CultureInfo.InvariantCulture, "{0}{{{1}}}", genericTypeName,
+                    string.Join(",", argumentTypeNames));
             }
             if (type.IsNested)
             {
