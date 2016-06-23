3cff473da78dbdde4caef941735759a486675c54
Anton Burkin <ramzzzay@gmail.com>
2016-06-23 10:39:20 +0300

    Refactoring: HelpPageAreaRegistration to C# 6 style

--- PhoneBook.Core/Areas/HelpPage/HelpPageAreaRegistration.cs
+++ PhoneBook.Core/Areas/HelpPage/HelpPageAreaRegistration.cs
@@ -5,20 +5,14 @@ namespace PhoneBook.Core.Areas.HelpPage
 {
     public class HelpPageAreaRegistration : AreaRegistration
     {
-        public override string AreaName
-        {
-            get
-            {
-                return "HelpPage";
-            }
-        }
+        public override string AreaName => "HelpPage";
 
         public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
                 "HelpPage_Default",
                 "Help/{action}/{apiId}",
-                new { controller = "Help", action = "Index", apiId = UrlParameter.Optional });
+                new {controller = "Help", action = "Index", apiId = UrlParameter.Optional});
 
             HelpPageConfig.Register(GlobalConfiguration.Configuration);
         }
