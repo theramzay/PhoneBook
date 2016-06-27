5a8ab898a2cb966e04b0daf118004def9aacf695
Anton Burkin <ramzzzay@gmail.com>
2016-06-27 10:48:08 +0300

    Refactoring: HelpPageSampleKey to C# 6 style

--- PhoneBook.Core/Areas/HelpPage/SampleGeneration/HelpPageSampleKey.cs
+++ PhoneBook.Core/Areas/HelpPage/SampleGeneration/HelpPageSampleKey.cs
@@ -6,12 +6,12 @@ using System.Net.Http.Headers;
 namespace PhoneBook.Core.Areas.HelpPage
 {
     /// <summary>
-    /// This is used to identify the place where the sample should be applied.
+    ///     This is used to identify the place where the sample should be applied.
     /// </summary>
     public class HelpPageSampleKey
     {
         /// <summary>
-        /// Creates a new <see cref="HelpPageSampleKey"/> based on media type.
+        ///     Creates a new <see cref="HelpPageSampleKey" /> based on media type.
         /// </summary>
         /// <param name="mediaType">The media type.</param>
         public HelpPageSampleKey(MediaTypeHeaderValue mediaType)
@@ -21,14 +21,14 @@ namespace PhoneBook.Core.Areas.HelpPage
                 throw new ArgumentNullException("mediaType");
             }
 
-            ActionName = String.Empty;
-            ControllerName = String.Empty;
+            ActionName = string.Empty;
+            ControllerName = string.Empty;
             MediaType = mediaType;
             ParameterNames = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
         }
 
         /// <summary>
-        /// Creates a new <see cref="HelpPageSampleKey"/> based on media type and CLR type.
+        ///     Creates a new <see cref="HelpPageSampleKey" /> based on media type and CLR type.
         /// </summary>
         /// <param name="mediaType">The media type.</param>
         /// <param name="type">The CLR type.</param>
@@ -44,17 +44,19 @@ namespace PhoneBook.Core.Areas.HelpPage
         }
 
         /// <summary>
-        /// Creates a new <see cref="HelpPageSampleKey"/> based on <see cref="SampleDirection"/>, controller name, action name and parameter names.
+        ///     Creates a new <see cref="HelpPageSampleKey" /> based on <see cref="SampleDirection" />, controller name, action
+        ///     name and parameter names.
         /// </summary>
-        /// <param name="sampleDirection">The <see cref="SampleDirection"/>.</param>
+        /// <param name="sampleDirection">The <see cref="SampleDirection" />.</param>
         /// <param name="controllerName">Name of the controller.</param>
         /// <param name="actionName">Name of the action.</param>
         /// <param name="parameterNames">The parameter names.</param>
-        public HelpPageSampleKey(SampleDirection sampleDirection, string controllerName, string actionName, IEnumerable<string> parameterNames)
+        public HelpPageSampleKey(SampleDirection sampleDirection, string controllerName, string actionName,
+            IEnumerable<string> parameterNames)
         {
             if (!Enum.IsDefined(typeof(SampleDirection), sampleDirection))
             {
-                throw new InvalidEnumArgumentException("sampleDirection", (int)sampleDirection, typeof(SampleDirection));
+                throw new InvalidEnumArgumentException("sampleDirection", (int) sampleDirection, typeof(SampleDirection));
             }
             if (controllerName == null)
             {
@@ -76,14 +78,16 @@ namespace PhoneBook.Core.Areas.HelpPage
         }
 
         /// <summary>
-        /// Creates a new <see cref="HelpPageSampleKey"/> based on media type, <see cref="SampleDirection"/>, controller name, action name and parameter names.
+        ///     Creates a new <see cref="HelpPageSampleKey" /> based on media type, <see cref="SampleDirection" />, controller
+        ///     name, action name and parameter names.
         /// </summary>
         /// <param name="mediaType">The media type.</param>
-        /// <param name="sampleDirection">The <see cref="SampleDirection"/>.</param>
+        /// <param name="sampleDirection">The <see cref="SampleDirection" />.</param>
         /// <param name="controllerName">Name of the controller.</param>
         /// <param name="actionName">Name of the action.</param>
         /// <param name="parameterNames">The parameter names.</param>
-        public HelpPageSampleKey(MediaTypeHeaderValue mediaType, SampleDirection sampleDirection, string controllerName, string actionName, IEnumerable<string> parameterNames)
+        public HelpPageSampleKey(MediaTypeHeaderValue mediaType, SampleDirection sampleDirection, string controllerName,
+            string actionName, IEnumerable<string> parameterNames)
             : this(sampleDirection, controllerName, actionName, parameterNames)
         {
             if (mediaType == null)
@@ -95,60 +99,60 @@ namespace PhoneBook.Core.Areas.HelpPage
         }
 
         /// <summary>
-        /// Gets the name of the controller.
+        ///     Gets the name of the controller.
         /// </summary>
         /// <value>
-        /// The name of the controller.
+        ///     The name of the controller.
         /// </value>
-        public string ControllerName { get; private set; }
+        public string ControllerName { get; }
 
         /// <summary>
-        /// Gets the name of the action.
+        ///     Gets the name of the action.
         /// </summary>
         /// <value>
-        /// The name of the action.
+        ///     The name of the action.
         /// </value>
-        public string ActionName { get; private set; }
+        public string ActionName { get; }
 
         /// <summary>
-        /// Gets the media type.
+        ///     Gets the media type.
         /// </summary>
         /// <value>
-        /// The media type.
+        ///     The media type.
         /// </value>
-        public MediaTypeHeaderValue MediaType { get; private set; }
+        public MediaTypeHeaderValue MediaType { get; }
 
         /// <summary>
-        /// Gets the parameter names.
+        ///     Gets the parameter names.
         /// </summary>
-        public HashSet<string> ParameterNames { get; private set; }
+        public HashSet<string> ParameterNames { get; }
 
-        public Type ParameterType { get; private set; }
+        public Type ParameterType { get; }
 
         /// <summary>
-        /// Gets the <see cref="SampleDirection"/>.
+        ///     Gets the <see cref="SampleDirection" />.
         /// </summary>
-        public SampleDirection? SampleDirection { get; private set; }
+        public SampleDirection? SampleDirection { get; }
 
         public override bool Equals(object obj)
         {
-            HelpPageSampleKey otherKey = obj as HelpPageSampleKey;
+            var otherKey = obj as HelpPageSampleKey;
             if (otherKey == null)
             {
                 return false;
             }
 
-            return String.Equals(ControllerName, otherKey.ControllerName, StringComparison.OrdinalIgnoreCase) &&
-                String.Equals(ActionName, otherKey.ActionName, StringComparison.OrdinalIgnoreCase) &&
-                (MediaType == otherKey.MediaType || (MediaType != null && MediaType.Equals(otherKey.MediaType))) &&
-                ParameterType == otherKey.ParameterType &&
-                SampleDirection == otherKey.SampleDirection &&
-                ParameterNames.SetEquals(otherKey.ParameterNames);
+            return string.Equals(ControllerName, otherKey.ControllerName, StringComparison.OrdinalIgnoreCase) &&
+                   string.Equals(ActionName, otherKey.ActionName, StringComparison.OrdinalIgnoreCase) &&
+                   (MediaType == otherKey.MediaType || (MediaType != null && MediaType.Equals(otherKey.MediaType))) &&
+                   ParameterType == otherKey.ParameterType &&
+                   SampleDirection == otherKey.SampleDirection &&
+                   ParameterNames.SetEquals(otherKey.ParameterNames);
         }
 
         public override int GetHashCode()
         {
-            int hashCode = ControllerName.ToUpperInvariant().GetHashCode() ^ ActionName.ToUpperInvariant().GetHashCode();
+            var hashCode = ControllerName.ToUpperInvariant().GetHashCode() ^ ActionName.ToUpperInvariant().GetHashCode();
             if (MediaType != null)
             {
                 hashCode ^= MediaType.GetHashCode();
@@ -161,7 +165,7 @@ namespace PhoneBook.Core.Areas.HelpPage
             {
                 hashCode ^= ParameterType.GetHashCode();
             }
-            foreach (string parameterName in ParameterNames)
+            foreach (var parameterName in ParameterNames)
             {
                 hashCode ^= parameterName.ToUpperInvariant().GetHashCode();
             }
